be685dd4a1abf21527ec8dd494f48436d8362438
Melvin Bubini <melvin.bubini@gmail.com>
2024-03-25 11:22:13 +0100

    första utkast för klar

--- src/js/machup.js
+++ src/js/machup.js
@@ -24,7 +24,7 @@ document.getElementById("search-form").addEventListener("submit", function (even
 
         // Efter 2 sekunder, kör sökfunktionerna
         searchWatchmode(searchTerm);
-        searchOMDb(searchTerm);
+        // searchOMDb(searchTerm);
     }, 1500); // 1500 millisekunder motsvarar 1 sekund
 });
 
@@ -39,30 +39,6 @@ document.getElementById("deleteBtn").addEventListener("click", function reset() 
 });
 
 
-function searchOMDb(searchTerm) {
-    let movieUrl = `https://www.omdbapi.com/?t=${encodeURIComponent(searchTerm)}&apikey=a3023c8d`;
-
-    fetch(movieUrl)
-        .then(response => response.json())
-        .then(data => {
-            let omdbResults = document.getElementById("omdb-results");
-            omdbResults.innerHTML = ""; // Rensa tidigare resultat
-
-            if (data.Response === "True") {
-                let poster = data.Poster;
-
-                let posterImage = document.createElement("img");
-                posterImage.classList.add("poster");
-                posterImage.src = poster;
-                omdbResults.appendChild(posterImage);
-            } else {
-                let errorMessage = document.createElement("p");
-                errorMessage.textContent = "Ingen film hittad";
-                omdbResults.appendChild(errorMessage);
-            }
-        })
-        .catch(error => console.error("Något gick fel:", error));
-}
 
 function searchWatchmode(searchTerm) {
     let url = `https://api.watchmode.com/v1/search/?apiKey=nBSOd4NMOHJGhsWCrd5aQ1lKCSQJgehofRSszRIC&search_field=name&search_value=${encodeURIComponent(searchTerm)}`;
@@ -73,6 +49,8 @@ function searchWatchmode(searchTerm) {
             let watchmodeResults = document.getElementById("watchmode-results");
             let watchmodeTitle = document.getElementById("watchmode-title");
             let titleHeading = document.createElement("h1");
+            let imdb_id = []; // Skapa en array för att lagra IMDb ID:n
+            let moreInfo = document.createElement("h2");
 
             watchmodeTitle.innerHTML = ""; // Rensa tidigare resultat
             watchmodeResults.innerHTML = ""; // Rensa tidigare resultat
@@ -82,13 +60,38 @@ function searchWatchmode(searchTerm) {
 
             if (data.title_results && data.title_results.length > 0) {
                 data.title_results.forEach(title => {
+                    const name = searchTerm;
                     titleHeading.textContent = `Alla titlar med ${searchTerm}`;
                     watchmodeTitle.appendChild(titleHeading);
+
+
+                    moreInfo.textContent = "Klicka på en film för mer information"
+                    watchmodeTitle.appendChild(moreInfo);
+
                     let listItem = document.createElement("li");
                     listItem.classList.add("new-list-item");
                     listItem.textContent = title.name;
                     watchmodeResults.appendChild(listItem);
+
+                    let divItem = document.createElement("div");
+                    divItem.classList.add("omdb_results2");
+                    listItem.appendChild(divItem);
+
+                    const imdbIds = title.imdb_id;
+                    imdb_id.push(imdbIds); // Lägg till IMDb ID till arrayen
+
+                    // Lägg till händelselyssnare för klick på varje li-element
+                    listItem.addEventListener('click', function () {
+                        // Anropa searchOMDb-funktionen med motsvarande IMDb ID
+                        searchOMDb(imdbIds);
+                    });
                 });
+
+                // // Endast skriv ut IMDb ID för varje film när de är tillgängliga
+                // imdb_id.forEach(id => {
+                //     console.log(id);
+                // });
+                
             } else {
                 let listItem = document.createElement("li");
                 listItem.textContent = "Ingen film hittad";
@@ -96,5 +99,43 @@ function searchWatchmode(searchTerm) {
             }
         })
         .catch(error => console.error("Något gick fel med fetch:", error));
+}
+
+
+function searchOMDb(imdbIds) {
+    let movieUrl = `https://www.omdbapi.com/?i=${encodeURIComponent(imdbIds)}&apikey=a3023c8d`;
+
+    fetch(movieUrl)
+        .then(response => response.json())
+        .then(data => {
+            let omdbResults = document.getElementById("omdb-results");
+            omdbResults.innerHTML = ""; // Rensa tidigare resultat
+
+            if (data.Response === "True") {
+                let poster = data.Poster;
+                let posterImage = document.createElement("img");
+                posterImage.classList.add("poster");
+                posterImage.src = poster;
+                omdbResults.appendChild(posterImage);
+
+                let imdb_review = data.imdbRating;
+                let imdbRating = document.createElement("h2");
+                imdbRating.textContent = `IMDb rating: ${imdb_review}`;
+                imdbRating.classList.add("imdbRating");
+                omdbResults.appendChild(imdbRating);
+
+                let description = data.Plot;
+                let plot = document.createElement("p");
+                plot.textContent = `Beskrivning: ${description}`;
+                plot.classList.add("movieDescription");
+                omdbResults.appendChild(plot);
+
+            } else {
+                let errorMessage = document.createElement("p");
+                errorMessage.textContent = "Denna film finns ej på IMDB eller är en bok";
+                omdbResults.appendChild(errorMessage);
+            }
+        })
+        .catch(error => console.error("Något gick fel:", error));
 }
 
